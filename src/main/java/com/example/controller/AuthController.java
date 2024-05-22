52d0655255515c626cc4ac45db376f73da6a6fcb
Leonardo Mejía <leomejia646@gmail.com>
2024-05-21 21:47:25 -0600

    Actualización de AuthController

--- src/main/java/com/example/controller/AuthController.java
+++ src/main/java/com/example/controller/AuthController.java
@@ -5,6 +5,10 @@ import com.example.gestores.GestorUsuarios;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
@@ -40,6 +44,16 @@ public class AuthController {
             return ResponseEntity.ok("Inicio de sesión exitoso.");
         } else {
             return ResponseEntity.status(401).body("Nombre de usuario o contraseña incorrectos.");
+        }
+    }
+
+    @GetMapping("/{username}/recomendaciones")
+    public ResponseEntity<List<Map<String, Object>>> obtenerRecomendaciones(@PathVariable String username, @RequestParam int k) {
+        List<Map<String, Object>> recomendaciones = gestorUsuarios.recommendClubsKNN(username, k);
+        if (recomendaciones != null && !recomendaciones.isEmpty()) {
+            return ResponseEntity.ok(recomendaciones);
+        } else {
+            return ResponseEntity.noContent().build();
         }
     }
 }
