ad3b3ca887c947e09ffd8fe8e09cb7bd3ed13192
Leonardo Mejía <leomejia646@gmail.com>
2024-05-28 20:51:39 -0600

    Arreglos en recomendaciones de clubes

--- src/main/java/com/example/gestores/GestorUsuarios.java
+++ src/main/java/com/example/gestores/GestorUsuarios.java
@@ -94,30 +94,29 @@ public class GestorUsuarios {
             });
         }
     }
-
     public List<Map<String, Object>> recommendClubsKNN(String username) {
         try (Session session = driver.session()) {
             return session.readTransaction(new TransactionWork<List<Map<String, Object>>>() {
                 @Override
                 public List<Map<String, Object>> execute(Transaction tx) {
-                    var result = tx.run("MATCH (u:Usuario {username: $username})-[:INTERESA_EN]->(a:Accion)-[:ASOCIADO_CON]->(c:Club) " +
-                                    "RETURN DISTINCT c.nombre AS club, c.concurrencia AS concurrencia, c.horarios AS horarios, c.facilitador AS facilitador, c.lugar AS lugar",
-                            parameters("username", username));
+                    var result = tx.run("MATCH (u:Usuario {username: $username})-[:INTERESA_EN]->(a:Accion)-[:LE_PUEDE_GUSTAR]->(c:Club) " +
+                                    "RETURN DISTINCT c.nombre AS club, c.concurrencia AS concurrencia, c.horario AS horario, c.lugar AS lugar",
+                            parameters("username",username));
 
                     List<Map<String, Object>> recommendedClubs = new ArrayList<>();
                     while (result.hasNext()) {
                         var record = result.next();
-                        recommendedClubs.add(Map.of(
-                                "club", record.get("club").asString(),
-                                "concurrencia", record.get("concurrencia").asString(),
-                                "horarios", record.get("horarios").asList(),
-                                "facilitador", record.get("facilitador").asString(),
-                                "lugar", record.get("lugar").asString()
-                        ));
+                        recommendedClubs.add(Map.of("club", record.get("club").asString()));
+                    }
+
+                    System.out.println("Recomendaciones encontradas para " + username + ": " + recommendedClubs.size());
+                    for (Map<String, Object> club : recommendedClubs) {
+                        System.out.println("Club recomendado: " + club.get("club"));
                     }
                     return recommendedClubs;
                 }
             });
         }
     }
+
 }
